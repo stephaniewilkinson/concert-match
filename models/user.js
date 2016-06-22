7893d15bb85c160ca827e29ec38fe561fa2f6271
Adam Berro <adamberro@gmail.com>
2016-06-22 16:58:24 -0700

    implements oauth to log in and out

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: String,
+  providerId: String,
+  created: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('User', userSchema);
