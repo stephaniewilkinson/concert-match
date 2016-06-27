3cda7b7070d15da5ff24fe1a9a103fa1b90aa677
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-27 16:28:43 -0700

    users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,29 +1,13 @@
 var User = require("../models/user");
 
-
 var index = function(req, res, next){
-  User.find({}, function(err, users) {
-    if (err) {
-      res.json({message: err});
-    } else {
-      res.render('users/index', {users: users});
-    }
-  });
-};
-
-var show = function(req, res, next){
-  User.findById(req.params.id, function(err, user) {
-    if (err) {
-      res.json({message: 'Could not find user because ' + err});
-    } else if (!user) {
-      res.json({message: 'No user with this id.'});
-    } else {
-      res.render('users/show', {user: user});
-    }
-  });
+  if (req.user) {
+    res.render('index', { user: req.user });
+  } else {
+    res.redirect('/login');
+  }
 };
 
 module.exports = {
-  index: index,
-  show:  show
+  index:  index
 };
