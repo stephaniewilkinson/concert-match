a5c6c078bb315ac09ec79b60fa1885d4bbfd5b42
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-27 15:56:27 -0700

    writes user controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,28 @@
+var User = require("../models/user");
+
+var index = function(req, res, next){
+  User.find({}, function(err, users) {
+    if (err) {
+      res.json({message: err});
+    } else {
+      res.render('users/index', {users: users});
+    }
+  });
+};
+
+var show = function(req, res, next){
+  User.findById(req.params.id, function(err, user) {
+    if (err) {
+      res.json({message: 'Could not find user because ' + err});
+    } else if (!user) {
+      res.json({message: 'No user with this id.'});
+    } else {
+      res.render('users/show', {user: user});
+    }
+  });
+};
+
+module.exports = {
+  index: index,
+  show:  show
+};
