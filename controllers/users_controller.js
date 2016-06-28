7bb5a35a895988ba517280aa88070bad73de6d70
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-28 10:25:13 -0700

    allows artists to be passed through to the view

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,34 +1,33 @@
 var User = require("../models/user");
 var request = require('request');
+var artists;
 
-function cb(err, res, body) {
-  if (!err && res.statusCode == 200) {
-    // console.log(e);
-    var artists = JSON.parse(body);
-    var artistNames = [];
-    artists.items.forEach(artist => artistNames.push(artist.name));
-    console.log(artists);
-  }
-}
-
-var index = function(req, res, next){
+function index(req, res, next){
   if (req.user & req.session.spotifyData) {
-    res.render('index', { user: req.user, spotifyData: req.session.spotifyData });
+    res.render('index', { user: req.user });
   } else if (req.user) {
-    console.log('line 22' +   req.user.accessToken);
     var options = {
-      url: 'https://api.spotify.com/v1/me/top/artists?limit=20',
+      url: 'https://api.spotify.com/v1/me/top/artists?limit=5',
       headers: {
         'Authorization': 'Bearer ' + req.user.accessToken
         }
       };
-    // fetch data
-    request(options, cb);
-    // save data to req.session.spotifyData
-    res.render('index', { user: req.user, spotifyData: req.session.spotifyData });
+      request.get(options, function(err, resp, body) {
+        parseArtists(body);
+        console.log("artists:", artists);
+        // this works
+        console.log(artists);
+        res.render('index', { user: req.user, artists: artists });
+      });
   } else {
     res.redirect('/login');
   }
+};
+
+parseArtists = function(x){
+  artists = JSON.parse(x).items;
+  //works
+  // console.log(artists);
 };
 
 module.exports = {
