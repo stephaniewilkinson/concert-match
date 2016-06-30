a656f851a4e91074c17c923123c118a135ea23ec
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-29 19:35:10 -0700

    fixes user errors

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  name: {type: String, required: true},
+  name: {type: String},
   email: String,
   providerId: String,
   photo: String,
